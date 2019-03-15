3584373451b913a615ade28b77b76831698e8ac5
github username <github email address>
2019-03-15 10:46:40 -0700

    initial solution

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const testVar = {}
 
+testFunc()
+
 function testFunc() {
-  return "hi"
+  let likes = document.getElementsByClassName('like')
+  for (let el of likes) {
+    el.addEventListener('click', () => {
+      let forceFailure = Math.random() < .2
+      mimicServerCall('fakeurl', {forceFailure})
+      .then(() => {
+        let heart = el.getElementsByTagName('span')[0]
+        heart.style.color = 'red'
+        heart.textContent = '♥'
+      })
+      .catch(() => {
+        document.write('sadness')
+      })
+    })
+  }
 }
