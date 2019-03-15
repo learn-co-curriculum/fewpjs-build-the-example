04d46c037453ae57421a24cadd431da41d3de1e8
github username <github email address>
2019-03-15 10:50:23 -0700

    untoggleable hearts

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const testVar = {}
+const EMPTY_HEART = '♡'
+const FULL_HEART = '♥'
 
 testFunc()
 
@@ -6,12 +7,20 @@ function testFunc() {
   let likes = document.getElementsByClassName('like')
   for (let el of likes) {
     el.addEventListener('click', () => {
+      let heart = el.getElementsByTagName('span')[0]
+
+      if (heart.textContent === FULL_HEART) {
+        heart.textContent = EMPTY_HEART
+        heart.style.color = ''
+        return
+      }
+
       let forceFailure = Math.random() < .2
       mimicServerCall('fakeurl', {forceFailure})
       .then(() => {
         let heart = el.getElementsByTagName('span')[0]
         heart.style.color = 'red'
-        heart.textContent = '♥'
+        heart.textContent = FULL_HEART
       })
       .catch(() => {
         document.write('sadness')
