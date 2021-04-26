fb1ba6348ba46602ce98c6aebdc656f98d05d824
musiccity365 <m_sackstein@yahoo.com>
2021-04-25 22:13:04 -0400

    Done.

--- main.js
+++ main.js
@@ -4,22 +4,49 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let glyphStates = {
+    "♡": "♥",
+    "♥": "♡"
+};
 
+let colorStates = {
+    "red": "",
+    "": "red"
+};
+
+let articleHearts = document.querySelectorAll(".like");
+
+function likeCallback(e) {
+    let heart = e.target;
+    mimicServerCall("bogusUrl")
+        //OR: mimicServerCall("bogusUrl", {forceFailure: true})
+        .then(function(serverMessage) {
+            heart.innerText = glyphStates[heart.innerText];
+            heart.style.color = colorStates[heart.style.color];
+        })
+        .catch(function(error) {
+            alert("Something went wrong!");
+        });
+}
+
+for (let glyph of articleHearts) {
+    glyph.addEventListener("click", likeCallback);
+}
 
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
-  });
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            let isRandomFailure = Math.random() < .2
+            if (isRandomFailure) {
+                reject("Random server error. Try again.");
+            } else {
+                resolve("Pretend remote server notified of action!");
+            }
+        }, 300);
+    });
 }
